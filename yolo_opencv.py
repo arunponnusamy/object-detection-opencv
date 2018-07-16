f2365938217a34a266e646a39d375837922caa9f
Arun Ponnusamy <arunponnusamy@hotmail.com>
2018-07-16 17:27:46 +0530

    Add yolo opencv object detection

---
+++ yolo_opencv.py
@@ -0,0 +1,98 @@
+#############################################
+# Object detection - YOLO - OpenCV
+# Author : Arun Ponnusamy   (July 16, 2018)
+# Website : http://www.arunponnusamy.com
+############################################
+
+
+import cv2
+import argparse
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+                help = 'path to input image')
+ap.add_argument('-c', '--config', required=True,
+                help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required=True,
+                help = 'path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True,
+                help = 'path to text file containing class names')
+args = ap.parse_args()
+
+
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), (0,255,0), 2)
+
+    label = str(classes[class_id])
+
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+
+
+net = cv2.dnn.readNet(args.weights, args.config)
+
+classes = None
+
+with open(args.classes, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+image = cv2.imread(args.image)
+
+image = cv2.resize(image, (416,416))
+
+blob = cv2.dnn.blobFromImage(image, 0.00392, (416,416), (0,0,0), True, crop=False)
+
+net.setInput(blob)
+
+outs = net.forward(get_output_layers(net))
+
+class_ids = []
+confidences = []
+boxes = []
+Width = 416
+Height = 416
+
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0] * Width)
+            center_y = int(detection[1] * Height)
+            w = int(detection[2] * Width)
+            h = int(detection[3] * Height)
+            x = center_x - w / 2
+            y = center_y - h / 2
+            class_ids.append(class_id)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+
+
+indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+for i in indices:
+    i = i[0]
+    box = boxes[i]
+    x = box[0]
+    y = box[1]
+    w = box[2]
+    h = box[3]
+    draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+cv2.imshow("object detection", image)
+cv2.waitKey()
+    
+cv2.imwrite("object-detection.jpg", image)
+cv2.destroyAllWindows()
