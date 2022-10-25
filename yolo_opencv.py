2833ce31fd46a768f4504e15210fc33ea701012e
Arun Ponnusamy <arunponnusamy@hotmail.com>
2022-10-25 13:30:55 +0530

    Fix invalid index issue

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -25,7 +25,7 @@ def get_output_layers(net):
     
     layer_names = net.getLayerNames()
     
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
     return output_layers
 
@@ -89,7 +89,6 @@ for out in outs:
 indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
 for i in indices:
-    i = i[0]
     box = boxes[i]
     x = box[0]
     y = box[1]
